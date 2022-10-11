ea4c9fd6f980957c56e5b2b9f99483bafd9f5d53
F56 <tomas950923@gmail.com>
2022-10-11 18:26:08 -0500

    init commit

---
+++ lint-staged.config.js
@@ -0,0 +1,15 @@
+export default {
+    // this will check Typescript files
+    "**/*.(ts)": () => "yarn tsc --noEmit",
+
+    // This will lint and format TypeScript and
+    // JavaScript files
+    "**/*.(ts|js)": filenames => [
+        `yarn eslint --fix ${filenames.join(" ")}`,
+        `yarn prettier --write ${filenames.join(" ")}`,
+    ],
+
+    // this will Format MarkDown and JSON
+    "**/*.(md|json)": filenames =>
+        `yarn prettier --write ${filenames.join(" ")}`,
+};
